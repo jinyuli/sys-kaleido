2c772d53b87c9c20f9a9ca9b56f211edf9819584
jinyu.li <jinyu.li@outlook.com>
2024-03-07 17:32:19 +0800

    format code

--- src/action/upgrade.rs
+++ src/action/upgrade.rs
@@ -2,32 +2,32 @@ use crate::tool::logger::error;
 
 pub fn upgrade() {
     tokio::task::spawn_blocking(|| {
-    let release_update = match self_update::backends::github::Update::configure()
-        .repo_owner("jinyuli")
-        .repo_name("sys-kaleido")
-        .bin_name("sys-kaleido")
-        .show_download_progress(true)
-        .current_version(self_update::cargo_crate_version!())
-        .build()
-    {
-        Ok(u) => u,
-        Err(e) => {
-            error!("failed to check update: {}", e);
-            return;
-        }
-    };
-    match release_update.update() {
-        Ok(s) => match s {
-            self_update::Status::UpToDate(v) => {
-                println!("sys-kaleido {} is the latest version.", v);
+        let release_update = match self_update::backends::github::Update::configure()
+            .repo_owner("jinyuli")
+            .repo_name("sys-kaleido")
+            .bin_name("sys-kaleido")
+            .show_download_progress(true)
+            .current_version(self_update::cargo_crate_version!())
+            .build()
+        {
+            Ok(u) => u,
+            Err(e) => {
+                error!("failed to check update: {}", e);
+                return;
             }
-            self_update::Status::Updated(v) => {
-                println!("sys-kaleido has been updated to {}.", v);
+        };
+        match release_update.update() {
+            Ok(s) => match s {
+                self_update::Status::UpToDate(v) => {
+                    println!("sys-kaleido {} is the latest version.", v);
+                }
+                self_update::Status::Updated(v) => {
+                    println!("sys-kaleido has been updated to {}.", v);
+                }
+            },
+            Err(e) => {
+                error!("failed to upgrade: {}", e);
             }
-        },
-        Err(e) => {
-            error!("failed to upgrade: {}", e);
-        }
-    };
+        };
     });
 }
